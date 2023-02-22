b8f6c91abc1e410e30e6ce03c97a3d2ad75201a6
Rob Copeland <copexrob@amazon.com>
2023-02-21 23:19:15 -0500

    working on the rooms

--- src/content/roomDescriptions.ts
+++ src/content/roomDescriptions.ts
@@ -1,95 +1,119 @@
 export const roomDescriptions = [
   {
-    name: "Room 1",
-    readAloudText: "",
-    description: "Empty",
-  },
-  {
-    name: "Room 2",
-    readAloudText: "",
-    description: "Trapdoor leading to a hidden area",
-  },
-  {
-    name: "Room 3",
+    name: "1. Workshop",
     readAloudText:
-      "Entering the complex, you can see a large room, with stone walls, ceilings and floors.",
-    description: "Copper Cobra x 4",
+      "Long wooden workbenches line the walls, covered in dozens of half-finished projects. Several " +
+      "strange, specific tools are hanging from the wall on hooks.",
+    description:
+      "Nothing of value in this room, but investigation could reveal an employee handbook that mentions lock controls " +
+      "in the Boss' Office.",
   },
   {
-    name: "Room 4",
-    readAloudText: "",
-    description: "Falling Block Trap",
+    name: "2. Office 01",
+    readAloudText:
+      "This room contains a number of pairs of desks and chairs. There is a soft rug underfoot. Each desk " +
+      "has assorted books and papers, drawings, etc. There are doors in the E and W walls.",
+    description:
+      "One particular desk has a recessed button underneath. If its pushed, the floor under the desk opens, " +
+      "revealing a trap door. If someone falls into the trapdoor, they slide down a chute and end up dropping " +
+      "through the ceiling of room 15.",
   },
   {
-    name: "Room 5",
+    name: "3. Entrance Hall",
+    readAloudText:
+      "Entering the complex, you can see a large room, " +
+      "with stone walls, ceilings and floors. It smells of trash and faintly of oil. " +
+      "There are a pair of doors in the NW corner, and another door in the S wall. Skittering across the floor are a " +
+      "number of creatures. They look like steel ribbons but with a head, almost like snakes. They don't appear to notice you.",
+    description:
+      "This room holds four Copper Cobras. They do not immediately attack the party, unless threatened. They are" +
+      "programmed to clean up the areas of the complex. If left alone, they will finish their work and leave. After a round," +
+      "the party will potentially hear the door above close and lock. The door cannot be opened from the inside.",
+  },
+  {
+    name: "4. Reception",
+    readAloudText:
+      "The room contains a long desk that cuts the room in half and a dozen chairs. There are " +
+      "promotional materials littering the ground. You notice that the ceiling in this area seems very unstable. There " +
+      "are doors in the N, NW, and W walls.",
+    description:
+      "Examining the papers, the party will see an advertisement for a Gnomish Building Company called Tinker's " +
+      "Workshop. Moving behind the desk or moving anything in the room causes the ceiling to cave in (Falling Block Trap)",
+  },
+  {
+    name: "5. Lavatory 01",
     readAloudText: "",
     description: "Falling Blocks",
   },
   {
-    name: "Room 6",
+    name: "6. Maintenance",
     readAloudText: "",
     description: "Teleport",
   },
   {
-    name: "Room 7",
+    name: "7. Collection",
     readAloudText: "",
     description: "Gelatinous Cube",
   },
   {
-    name: "Room 8",
+    name: "8. Rear Collection",
     readAloudText: "",
     description: "Poison Gas Trap",
   },
   {
-    name: "Room 9",
+    name: "9. South Collection",
     readAloudText: "",
     description: "Copper Cobra x 6",
   },
   {
-    name: "Room 10",
+    name: "10. Warehouse",
     readAloudText: "",
     description: "Animated Object",
   },
   {
-    name: "Room 11",
+    name: "11. Lab",
     readAloudText: "",
     description: "Copper Cobra x 4",
   },
   {
-    name: "Room 12",
+    name: "12. Cafeteria",
     readAloudText: "",
     description:
-      "The room contains a stone fountain containing clear water. Drinking even a small amount grants 1d4 healing once per character per day.",
+      "The room contains a stone fountain containing clear water. Drinking even a small amount grants 1d4 healing " +
+      "once per character per day.",
   },
   {
-    name: "Room 13",
+    name: "13. Open Office",
     readAloudText: "",
     description: "Mist trap",
   },
   {
-    name: "Room 14",
+    name: "14. Sanitation",
     readAloudText: "",
     description: "Empty",
   },
   {
-    name: "Room 15",
+    name: "15. Sanitation",
     readAloudText: "",
     description: "Alarm",
   },
   {
-    name: "Room 16",
-    readAloudText: "",
+    name: "16. Lavatory 02",
+    readAloudText:
+      "The room contains a few 5ft stalls, each with their own door. The stalls don't seem to have been disturbed, but this room has clearly been lived in for a while. Around the room, " +
+      "you see some bedrolls, ration containers and a makeshift weapon repair bench.",
     description:
-      "In this room, a group of adventurers have made a camp. Several bedrolls are rolled out and some ration containers are strewn about.",
+      "This is the camp of the Adventuring party. If any of the members are here, they can talk to the group. They are willing to " +
+      "go along with pretty much any plan that means they get to escape.",
   },
   {
-    name: "Room 17",
+    name: "17. Storage",
     readAloudText: "",
     description:
       "The room contains a stone fountain containing black, stinking liquid. Drinking it means a Save v Poison, touching it does 1d4 damage.",
   },
   {
-    name: "Room 18",
+    name: "18. Private Office",
     readAloudText: "",
     description: "Empty",
   },
